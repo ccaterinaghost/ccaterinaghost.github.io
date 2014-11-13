402cf2fdde02966e15962327a6ce865473dad7d8
ccaterinaghost <ccat.x@Hotmail.com>
2014-11-13 08:16:56 -0500

    moved folders

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var $win = $('window');
+var $logo = $('.logosection');
+var $about = $('.about');
+var $links = $('.links');
+var $portfolio = $('.portfolio')
+
+$about.waypoint(function (direction) {
+	if (direction == 'down') {
+		$logo.addClass('js-logo-move');
+ 	} else {
+		$logo.removeClass('js-logo-move');
+	}
+}, {offset: '50%'});
