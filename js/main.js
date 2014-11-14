db6838ae6f369f15d58f26199d11e2251decd1af
ccaterinaghost <ccat.x@Hotmail.com>
2014-11-13 19:29:17 -0500

    blurring background

--- js/main.js
+++ js/main.js
@@ -11,15 +11,8 @@ $about.waypoint(function (direction) {
  	} else {
 		$logo.removeClass('js-logo-move');
 	}
-}, {offset: '50%'});
+}, {offset: '80%'});
 
-$contact.waypoint(function (direction) {
-	if (direction == 'down') {
-		$aboutpar.addClass('js-about-par-move');
- 	} else {
-		$aboutpar.removeClass('js-about-par-move');
-	}
-}, {offset: '500%'});
 
 $(document).ready(function() {
 	$(".fancybox").fancybox({
@@ -28,3 +21,36 @@ $(document).ready(function() {
 	});
 });
 
+$(function(){
+    zoom = $('.feature').css('background-size')
+    zoom = parseFloat(zoom)/100
+    size = zoom * $('.feature').width();
+    $(window).on('scroll', function(){
+      fromTop = $(window).scrollTop();
+      newSize = size - (fromTop/3);
+      if (newSize > $('.feature').width()) {
+          $('.feature').css({
+              '-webkit-background-size': newSize,
+              '-moz-background-size': newSize,
+              '-o-background-size': newSize,
+              'background-size': newSize,
+              '-webkit-filter':'blur('+ 0 + (fromTop/100) + 'px)',
+              'opacity': 1 - ((fromTop / $('html').height()) * 1.3)
+          });
+      }
+    });
+});
+
+$(function (){
+    var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+    var isSafari = /Safari/.test(navigator.userAgent) && /Apple Computer/.test(navigator.vendor);
+
+    if (isChrome || isSafari) {
+    } else {
+        $('.feature').append('<div class="opaque"></div>');
+        $(window).on('scroll', function(){
+            var opacity = 0 + ($(window).scrollTop()/5000);
+            $('.opaque').css('opacity', opacity);
+        });
+    }
+});
